850f005b051863a4f2d2c546c71906507678b0b5
ArnoldoZerecero <arnoldoz97@hotmail.com>
2017-06-06 01:18:35 -0500

    Add files via upload
    Simulación de una ALU en Verilog.

---
+++ ALU/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/Documents/UAG/ISE/ALU/ALU_isim_beh.exe" -prj "D:/Documents/UAG/ISE/ALU/ALU_beh.prj" "work.ALU" "work.glbl" 
